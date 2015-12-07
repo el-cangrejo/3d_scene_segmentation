0b32c8c7db54914fe0be706598bbfd71d591febd
DimitrisDimou <mijuomij@gmail.com>
2015-12-07 21:24:55 +0200

    Initial commit

---
+++ include/segmentation.hpp
@@ -0,0 +1,26 @@
+#ifndef SEGMENTATION_HPP_
+#define SEGMENTATION_HPP_
+
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/core/utility.hpp"
+#include "opencv2/highgui/highgui_c.h"
+
+#include <stdio.h>
+#include <iostream>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <initializer_list>
+
+using std::cout;
+
+using namespace cv;
+
+Mat image;			// Original Input Image
+
+#endif // SEGMENTATION_HPP_
